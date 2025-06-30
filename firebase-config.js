82e4633c0469ceeb2e1241ac7ab17c31259a4c85
ellycit.international.dao.1.10@gmail.com <131910208+EllyCitDao112@users.noreply.github.com>
2025-06-30 03:53:08 +0300

    Create firebase-config.js
    firebase-config.js

---
+++ firebase-config.js
@@ -0,0 +1,15 @@
+```javascript
+// Firebase config – replace with your values
+const firebaseConfig = {
+  apiKey: "YOUR_API_KEY",
+  authDomain: "YOUR_PROJECT.firebaseapp.com",
+  databaseURL: "https://YOUR_PROJECT.firebaseio.com",
+  projectId: "YOUR_PROJECT",
+  storageBucket: "YOUR_PROJECT.appspot.com",
+  messagingSenderId: "YOUR_SENDER_ID",
+  appId: "YOUR_APP_ID",
+};
+
+firebase.initializeApp(firebaseConfig);
+window.FIREBASE_APP = firebase;
+```
