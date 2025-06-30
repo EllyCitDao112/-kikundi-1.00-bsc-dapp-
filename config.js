60acb704457d98911a6d1b1a990710d03c23a789
ellycit.international.dao.1.10@gmail.com <131910208+EllyCitDao112@users.noreply.github.com>
2025-06-30 03:48:35 +0300

    Create config.js
    config.js

---
+++ config.js
@@ -0,0 +1,8 @@
+```javascript
+window.KIKUNDI_CONFIG = {
+  contractAddress: "0xYourStakingContractAddress",
+  tokenAddress: "0xYourKKTTokenAddress",
+  groupId: "kikundi-alpha",
+  chainId: "0x61", // BSC Testnet
+};
+```
